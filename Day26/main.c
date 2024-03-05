f487a27bb550482f114bc2f9f350b615b225bf47
Mary Princy Rajan <mailtoprincy2011@gmail.com>
2024-03-05 01:24:49 -0500

    Day26 code challenge

---
+++ Day26/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*power: raise base to n-th power;n >= 0*/
+int power(int base, int n)
+{
+	int i, result = 1;
+	for (i = 1; i <= n; i++)
+	{
+		result = result * base;
+	}
+	return result;
+}
+
+int main()
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%-2d %-4d %-4d\n", i, power(2, i), power(-3, i));
+	}
+	return 0;
+}
